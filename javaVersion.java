b7262a7a9eae259f51cd1e9002d6d17033544e06
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-01-10 22:45:57 -0800

    Jan.10th

---
+++ javaVersion.java
@@ -0,0 +1,130 @@
+import java.util.Stack;
+
+class Solution {
+    public class ListNode {
+       int val;
+        ListNode next;
+        ListNode(int x) { val = x; } 
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    //1108
+    public static String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+    //1281
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+        while(n>0) {
+            product *= (n%10);
+            sum += (n%10);
+            n/=10;
+        }
+        return (product - sum);
+    }
+    //1295
+    public static int findNumbers(int[] nums) {
+        int result = 0 ;
+        for (int i =0; i< nums.length; i++){
+            String s = Integer.toString(nums[i]);
+            if(s.length() % 2 == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+    //771
+    public int numJewelsInStones(String J, String S) {
+        char[] jewels = J.toCharArray();
+        char[] stones = S.toCharArray();
+        int found = 0;
+        l1: for(char s : stones) {
+            for(char j : jewels) {
+                if(s == j) {
+                    found++;
+                    continue l1;
+                }
+            }
+        }
+        
+        return found; 
+    }
+    //1290
+    public int getDecimalValue(ListNode head) {
+        String  combString = "";
+        while (head != null) {
+            combString += Integer.toString(head.val);
+            head = head.next;
+        }
+        int result = 0;
+        int bit = combString.length();
+        for (int i =0 ; i< combString.length(); i++) {
+            if (combString.charAt(i) == '1') {
+                result += Math.pow(2,bit-i-1);
+            }
+        }
+        return result;
+    }
+    //1221
+    public int balancedStringSplit(String s) {
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        for (int i =0 ; i< s.length(); i++) {
+            if (!stack.isEmpty()){
+                if(s.charAt(i) == 'L' && stack.peek() == 'R'){
+                    stack.pop();
+                    if (stack.isEmpty()){
+                        result ++;
+                    }
+                }else if(s.charAt(i) == 'R' && stack.peek() == 'L'){
+                    stack.pop();
+                    if (stack.isEmpty()){
+                        result ++;
+                    } 
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            }else {
+                stack.push(s.charAt(i));
+            }
+        }
+        return result;
+    }
+    public int balancedStringSplitAnotherFasterSolution(String s) {
+        int r=0,l=0;
+        int res=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='L'){l++;}
+            else{r++;}
+            if(r==l){
+                l=0;
+                r=0;
+                res++;
+            }
+        }
+        return res;
+    }
+    // 938
+    int result = 0;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root != null) {
+            rangeSumBST(root.left, L, R);
+            if (root.val <= R && root.val >= L) {
+                result += root.val;
+            }
+            rangeSumBST(root.right, L, R);
+        }
+        return result;
+
+    }
+    public static void main(String[] args) {
+        String input = "1.1.1.1";
+        int[] nums = {12,345,2,6,7896};
+        findNumbers(nums);
+    }
+}
