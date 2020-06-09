f16e418e8654dcde44a3c8eea0cf9fc11f00b8b9
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-06-09 16:39:50 -0700

    Jun.9th good afternoon

--- top100.py
+++ top100.py
@@ -31,13 +31,14 @@ class Solution(object):
                     recursion(currNode.right, newD)
         recursion(root, initDepth)
         return max(depth)
+
     def maxDepthAnotherVersion(self, root):
         if(root is None):
             return 0
         else:
-            return 1 + max (self.maxDepthAnotherVersion(root.left), self.maxDepthAnotherVersion(root.right))
+            return 1 + max(self.maxDepthAnotherVersion(root.left), self.maxDepthAnotherVersion(root.right))
 
-    #136 Single Number should have a runtime complexity without using extra memory
+    # 136 Single Number should have a runtime complexity without using extra memory
     def singleNumber(self, nums):
         """
         :type nums: List[int]
@@ -48,24 +49,26 @@ class Solution(object):
                 continue
             else:
                 return num
+
     def singleNumber1684ms(self, nums):
-        copy =[]
+        copy = []
         for num in nums:
             if num in copy:
                 copy.remove(num)
             else:
                 copy.append(num)
         return copy[0]
+
     def singleNumber80ms(self, nums):
-        dic= {}
+        dic = {}
         for num in nums:
             if num not in dic:
                 dic[num] = 1
             else:
                 dic.pop(num)
         return dic.keys()[0]
-        
-    #206
+
+    # 206
     def reverseList(self, head):
         """
         :type head: ListNode
@@ -80,3 +83,43 @@ class Solution(object):
             curr = Next
         head = prev
         return head
+
+    # 169
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
+        for key in dic.keys():
+            if dic[key] > len(nums)/2:
+                return key
+
+    # 283
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        count = nums.count(0)
+        while (count != 0):
+            for i in range(len(nums)-1):
+                if nums[i] == 0 and nums[i+1] != 0:
+                    nums[i] = nums[i+1]
+                    nums[i+1] = 0
+                elif nums[i] == 0 and nums[i+1] == 0:
+                    continue
+            count -= 1
+
+    # 448
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
