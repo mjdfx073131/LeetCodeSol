d3ca113a05bbe1514ee2605d488d7ff439bb26d2
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-06-09 22:09:44 -0700

    Jun.9th good night!

--- top100.py
+++ top100.py
@@ -6,6 +6,12 @@ class TreeNode(object):
         self.right = right
 
 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
     # 104 Maximum depth of binary tree
 
@@ -116,10 +122,95 @@ class Solution(object):
                     continue
             count -= 1
 
-    # 448
+    # 448 find disappear number in an array
     def findDisappearedNumbers(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        
+        check = 1
+        dic = {}
+        result = []
+        for i in range(len(nums)):
+            dic[i+1] = False
+        for num in nums:
+            dic[num] = True
+
+        for key in dic.keys():
+            if dic[key] == False:
+                result.append(key)
+        return result
+
+    # 21 merge two sorted lists
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(None)
+        curr = head
+        left = l1
+        right = l2
+        while (left != None and right != None):
+            newLeft = ListNode(left.val)
+            newRight = ListNode(right.val)
+            if left.val < right.val:
+                curr.next = newLeft
+                curr = curr.next
+                left = left.next
+            elif right.val < left.val:
+                curr.next = newRight
+                curr = curr.next
+                right = right.next
+            elif right.val == left.val:
+                curr.next = newLeft
+                curr.next.next = newRight
+                curr = curr.next.next
+                right = right.next
+                left = left.next
+        while (left != None):
+            curr.next = ListNode(left.val)
+            left = left.next
+            curr = curr.next
+        while (right != None):
+            curr.next = ListNode(right.val)
+            right = right.next
+            curr = curr.next
+        return head.next
+
+    # 121 Best tiem to buy and sell stock
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if (len(prices) == 0):
+            return 0
+        if (len(prices) == 1):
+            return 0
+        if(all(prices[i] >= prices[i+1] for i in range(len(prices)-1))):
+            return 0
+        maxProfit = 0
+        for i in range(len(prices)-1):
+            buy = prices[i]
+            for j in range(i+1, len(prices)):
+                if (buy < prices[j]):
+                    profit = prices[j] - buy
+                    if (profit > maxProfit):
+                        maxProfit = profit
+        return maxProfit
+
+    def maxProfitBetterSolution(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        minPrice = float('inf')
+        maxProfit = 0
+        for price in prices:
+            if price < minPrice:
+                minPrice = price
+            elif price - minPrice > maxProfit:
+                maxProfit = price - minPrice
+        return maxProfit
