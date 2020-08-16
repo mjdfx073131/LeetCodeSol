8f8739869c973e9cbde590b5eeabe19a2a578b14
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-08-15 21:51:22 -0700

    Aug.15th good night

--- top100.py
+++ top100.py
@@ -316,4 +316,21 @@ class Solution(object):
         return maxSub
 
 
-        
+
+    #1. Two Sum
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in range (len(nums)):
+            diff = target - nums[i]
+            if diff in d:
+                return[d[diff], i]
+            d[num[i]] = i
+
+            
+
+
